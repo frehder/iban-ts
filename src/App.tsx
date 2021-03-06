9a323741caeae1d6d9372c0e8a1abc00d7903cf3
frehder <hi@florianrehder.de>
2021-03-06 18:29:40 +0100

    update app component with ibaninput

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import IbanInput from './components/IbanInput';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <IbanInput inputStyle={{ padding: '5px 15px', fontSize: 16 }} placeholder="DE12 1234 1234 1234 1234 12" />
+        </div>
+    );
 }
 
 export default App;
